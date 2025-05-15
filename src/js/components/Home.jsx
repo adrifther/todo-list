dbbc297cc0787c652eaab3564fa7d2289bf3602f
adrifther <adrianferrert@gmail.com>
2025-05-15 17:40:56 +0000

    first commit

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,26 +1,39 @@
-import React from "react";
+import React, {useState} from "react";
+
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+	const[ inputValue, setInputValue ] = useState("hola");
+	const[ todos, setTodos ] = useState([]);	
+console.log(todos);
+	return (
+		<div className="container">
+			<h1>todos</h1>
+			<ul>
+				<li>
+					<input 
+						type="text" 
+						onChange={(event)=>setInputValue(event.target.value)}
+						value={inputValue}
+						onKeyDown= {(e)=> e.key === 'Enter' ? setTodos(todos.concat(inputValue)) : null}
+						placeholder="What needs to be done?"/>
+				</li>
+					{todos.map((todo, index) => (<li key={index}><div className="d-flex justify-content-between">
+					<div>{todo}</div>
+					<div 
+						onClick={()=>setTodos(todos.filter((_,currentIndex)=> index != currentIndex))}>
+					X
+					</div>
+					</div>
+				</li>))}
+			</ul>
+			<div>
+				{todos.length} tasks
+			</div>
 		</div>
 	);
 };
